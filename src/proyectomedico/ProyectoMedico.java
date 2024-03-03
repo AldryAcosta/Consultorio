2adb5f9f395c392d7fd2b0b65ee1ca6042c37f23
Aldry Acosta <aldryjose2003@gmail.com>
2024-03-03 16:29:47 -0500

    Version 2

--- src/proyectomedico/ProyectoMedico.java
+++ src/proyectomedico/ProyectoMedico.java
@@ -14,7 +14,7 @@ public class ProyectoMedico {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Matame jorman, no aguanto mas esta agonia con github");
     }
     
 }
