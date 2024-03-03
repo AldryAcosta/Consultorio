6ae68de5b8264a4c569fd8aadcbb338f09b394b9
Aldry Acosta <aldryjose2003@gmail.com>
2024-03-03 17:18:48 -0500

    De vuelta a la version 1 (sin nada)

--- src/proyectomedico/ProyectoMedico.java
+++ src/proyectomedico/ProyectoMedico.java
@@ -14,7 +14,7 @@ public class ProyectoMedico {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Matame jorman, no aguanto mas esta agonia con github");
+        
     }
     
 }
